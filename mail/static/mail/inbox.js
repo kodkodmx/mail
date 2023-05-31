7edbaf46c3382cd1bb019cd372db13c28714b8ab
Alberto Haboba <betoh@hotmail.com>
2023-05-30 20:16:46 -0600

    sned email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,7 +20,27 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // Send email
+  document.querySelector('#compose-form').onsubmit = () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    })
+    .then(() => load_mailbox('sent'));
+    return false;
+  }
 }
+
 
 function load_mailbox(mailbox) {
   
@@ -30,4 +50,6 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  
 }
